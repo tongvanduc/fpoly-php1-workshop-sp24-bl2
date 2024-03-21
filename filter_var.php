6d9971401cec1a5047a30e60ce50533c71690080
h2oXD <fixbugandcry@gmail.com>
2024-03-21 22:59:20 +0700

    session3

--- filter_var.php
+++ filter_var.php
@@ -1,35 +1,41 @@
 <?php 
-if(!function_exists('debug')){
-    function debug($data){
-        echo "<pre>";
+// Viết 1 hoặc nhiều hàm hỗ trợ đc việc là validate dữ liệu theo yêu cầu sử dụng filter_var
+session_start();
 
-        print_r($data);
+echo "<pre>";
 
-        die;
+// Bắt buộc có dữ liệu
+function required($field, $value) {
+    if (empty($value)) {
+        $_SESSION['errors'][] = "Trường $field là bắt buộc";
     }
 }
-// Viết 1 hoặc nhiều hàm hỗ trợ đc việc là validate dữ liệu theo yêu cầu sử dụng filter_var
-// $data = [
-//     'email' => 'ahihi@gmail.com',
-//     'home_ip' => '1.2.5.6'
-// ];
 
-// validate([
-//     'title' => 'required|email',
-//     'home_ip' => 'required|ip',
-// ], $data);
+// Email
+function email($field, $value) {
+    if (! filter_var($value, FILTER_VALIDATE_EMAIL)) {
+        $_SESSION['errors'][] = "Trường $field không đúng định dạng email";
+    }
+}
+
+// IP
+function ip($field, $value) {
+    if (! filter_var($value, FILTER_VALIDATE_IP)) {
+        $_SESSION['errors'][] = "Trường $field không đúng định dạng IP";
+    }
+}
 $data = [
     'email' => 'ahihi@gmail.com',
     'home_ip' => '1.2.5.6'
 ];
 
-$rule = [
-    'title' => 'required|email',
+$rules = [
+    'email' => 'required|email',
     'home_ip' => 'required|ip',
 ];
 
 
-validate($rule,$data);
+validate($rules,$data);
 function validate($rules, $data) {
     foreach ($data as $field => $value) {
         $rule = $rules[$field];

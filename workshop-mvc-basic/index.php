7218ae10f4b4003a1748e39fb938248d5e8dd313
Tống Văn Đức <tongduc315@gmail.com>
2024-03-27 22:01:20 +0700

    update mvc

---
+++ workshop-mvc-basic/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+// Khởi tạo ứng dụng: rquire file controllers, models, connectdb, commons 
+// Điều hướng request
+
+// commons
+require_once './commons/helper.php';
+
+// models
+require_file('./models/');
+
+// controllers
+require_file('./controllers/');
+
+// Điều hướng
+$act = $_GET['act'] ?? '/';
+
+match($act) {
+    '/' => homeShow(),
+
+    'txthint' => homeTxtHint(),
+};
+
